f0d836f8bca08b9c8a829f4ac2f0b458c261b70e
Jocke <zedjay88@gmail.com>
2022-11-20 13:32:49 +0100

    .

--- src/main/java/com/example/easynotes/controller/IndexController.java
+++ src/main/java/com/example/easynotes/controller/IndexController.java
@@ -1,9 +1,10 @@
 package com.example.easynotes.controller;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.easynotes.model.Canvas;
 
 @RestController
 @RequestMapping("/")
